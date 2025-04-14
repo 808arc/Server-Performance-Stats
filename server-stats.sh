d399f19b598d516b191a0a207e9c74e470accb90
808arc <808arc>
2025-04-13 20:08:57 -0400

    I need to learn shell scripting

--- server-stats.sh
+++ server-stats.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+top -bn1 | grep "Cpu(s)" > output.txt  
 
-top -bn1 | grep "Cpu(s)"> output.txt  
+# Extract memory usage and calculate percentages
+free -h -t | awk '/^Mem:/ {
+    total=$2; used=$3; free=$4;
+    used_percent=($3/$2)*100;
+    free_percent=($4/$2)*100;
+    printf "Memory Usage: Total: %s, Used: %s (%.2f%%), Free: %s (%.2f%%)\n", total, used, used_percent, free, free_percent;
+}' >> output.txt
+
+#df -h -T | grep "Filesystem" >> output.txt
