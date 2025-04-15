066c72511716748322c8eaa88c3e26d92f6a045c
808arc <808arc>
2025-04-15 13:25:57 -0400

    function declaration

--- server-stats.sh
+++ server-stats.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-top -bn1 | grep "Cpu(s)" > output.txt  
+touch stats.txt
 
-# Extract memory usage and calculate percentages
-free -h -t | awk '/^Mem:/ {
-    total=$2; used=$3; free=$4;
-    used_percent=($3/$2)*100;
-    free_percent=($4/$2)*100;
-    printf "Memory Usage: Total: %s, Used: %s (%.2f%%), Free: %s (%.2f%%)\n", total, used, used_percent, free, free_percent;
-}' >> output.txt
+cpu_usage()
 
-#df -h -T | grep "Filesystem" >> output.txt
+mem_usage()
+
+disk_usage()
+
+top5_cpu_processes()
+top5_mem_processes()
